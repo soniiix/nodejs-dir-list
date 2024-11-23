93339af4e1f2d962a749833cb5b905a677a02d2a
soniiix <quent.couzinet@gmail.com>
2024-11-23 21:00:28 +0100

    feat: ensure the specified path is valid

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import { argv } from 'node:process';
+import path from 'node:path';
+import { existsSync } from 'node:fs';
 
 if (argv.length != 3){
     argv.length < 3
@@ -6,5 +8,14 @@ if (argv.length != 3){
         : console.error("Error: Too many arguments provided. Only one directory path expected.");
     process.exit(1)
 } else {
-    //continue process...
+    const relativePath = argv[2];
+    const absolutePath = path.resolve(process.cwd(), relativePath);
+
+    if (!existsSync(absolutePath)){
+        console.log("Error: The specified path was not found.")
+        process.exit(1)
+    }
+    else{
+        //continue process...
+    }
 }
