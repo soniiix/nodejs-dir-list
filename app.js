6268d2e6e3ace270295c6e7378e93947e27e4e1f
soniiix <quent.couzinet@gmail.com>
2024-11-24 00:22:33 +0100

    feat: list files and directories with size

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { argv } from 'node:process';
 import path from 'node:path';
 import { existsSync } from 'node:fs';
+import { readdir, stat } from 'node:fs/promises';
 
 if (argv.length != 3){
     argv.length < 3
@@ -16,6 +17,17 @@ if (argv.length != 3){
         process.exit(1)
     }
     else{
-        //continue process...
+        const files = await readdir(absolutePath, {
+            withFileTypes: true
+        });
+        for (const file of files){
+            const stats = await stat(path.join(absolutePath, file.name))
+            console.log(
+                file.isDirectory() ? "DIR." : "FILE",
+                " - ",
+                file.name,
+                file.isFile() ? ' -  ' + stats.size + 'ko' : ''
+            )
+        }
     }
 }
