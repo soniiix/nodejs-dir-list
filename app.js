b2f35e50e07d1f187147855fa37aa281615bbe56
soniiix <quent.couzinet@gmail.com>
2024-11-24 16:03:16 +0100

    style: integrate chalk package for styling console output

--- app.js
+++ app.js
@@ -2,24 +2,32 @@ import { argv } from 'node:process';
 import path from 'node:path';
 import { existsSync } from 'node:fs';
 import { readdir, stat } from 'node:fs/promises';
+import chalk from 'chalk';
+
+//chalk themes
+const error = chalk.red.bold
 
 if (argv.length != 3){
     argv.length < 3
-        ? console.error("Error: Missing path argument. Please provide a directory path.")
-        : console.error("Error: Too many arguments provided. Only one directory path expected.");
+        ? console.error(error("Error: Missing path argument. Please provide a directory path."))
+        : console.error(error("Error: Too many arguments provided. Only one directory path expected."));
     process.exit(1)
-} else {
+}
+else {
     const relativePath = argv[2];
     const absolutePath = path.resolve(process.cwd(), relativePath);
 
     if (!existsSync(absolutePath)){
-        console.error("Error: The specified path was not found.")
+        console.error(error("Error: The specified path was not found."))
         process.exit(1)
     }
-    else{
+    else {
         const files = await readdir(absolutePath, {
             withFileTypes: true
         });
+
+        console.log(`\nContent of ${chalk.blue.underline(absolutePath)} :\n`)
+
         for (const file of files){
             const stats = await stat(path.join(absolutePath, file.name))
 
