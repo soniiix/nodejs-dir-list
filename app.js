7cef8398af2e4993e8e76260ada9af6db080842e
soniiix <quent.couzinet@gmail.com>
2024-11-25 14:48:01 +0100

    feat: handle case when the specified directory is empty

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ import { readdir, stat } from 'node:fs/promises';
 import chalk from 'chalk';
 
 //chalk themes
-const error = chalk.red.bold
+const error = chalk.red.bold;
+const warning = chalk.yellow.bold;
 
 if (argv.length != 3){
     argv.length < 3
@@ -26,18 +27,23 @@ else {
             withFileTypes: true
         });
 
-        console.log(`\nContent of ${chalk.blue.bold.underline(absolutePath)} :\n`)
+        if (files.length == 0){
+            console.log(warning("Warning: The specified directory is empty."))
+        }
+        else {
+            console.log(`\nContent of ${chalk.blue.bold.underline(absolutePath)} :\n`)
 
-        for (const file of files){
-            const stats = await stat(path.join(absolutePath, file.name))
+            for (const file of files){
+                const stats = await stat(path.join(absolutePath, file.name))
 
-            const lastModifiedTime = new Date(stats.mtime).toLocaleString();
-            const type = file.isDirectory() ? "DIR." : "FILE";
-            const fileName = file.isDirectory() ? chalk.blue(file.name.padEnd(20)) : file.name.padEnd(20);
-            const size = file.isFile() ? `${stats.size} B` : '-';
+                const lastModifiedTime = new Date(stats.mtime).toLocaleString();
+                const type = file.isDirectory() ? "DIR." : "FILE";
+                const fileName = file.isDirectory() ? chalk.blue(file.name.padEnd(20)) : file.name.padEnd(20);
+                const size = file.isFile() ? `${stats.size} B` : '-';
 
-            //output
-            console.log(`${lastModifiedTime}    ${chalk.bold(type)}    ${fileName}    ${size}`)
+                //output
+                console.log(`${lastModifiedTime}    ${chalk.bold(type)}    ${fileName}    ${size}`)
+            }
         }
     }
 }
